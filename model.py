228273fd0db8cedc3a91cbf3450cda00266c75b5
PradhumnSrivastava <srivastavapradhumn@gmail.com>
2026-03-23 20:05:25 +0530

    Lazy loading model fix

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 from transformers import pipeline
 
-classifier = pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english",
-    device=-1
-)
+classifier = None
 
 def predict_sentiment(text):
+    global classifier
+
+    if classifier is None:
+        classifier = pipeline(
+            "sentiment-analysis",
+            model="distilbert-base-uncased-finetuned-sst-2-english",
+            device=-1
+        )
+
     result = classifier(text)[0]
     return result["label"], result["score"]
